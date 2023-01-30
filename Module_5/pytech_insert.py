9d52d9052f7cfb848c4c1e251ea788fbe928cc3f
steffen-albright <104539115+steffen-albright@users.noreply.github.com>
2023-01-30 09:55:16 -0600

    Update pytech_insert.py

--- Module_5/pytech_insert.py
+++ Module_5/pytech_insert.py
@@ -1,32 +1,40 @@
 from pymongo import MongoClient
-
+ 
 url = "mongodb+srv://admin:admin@cluster0.7vyzi7i.mongodb.net/?retryWrites=true&w=majority"
 
 client = MongoClient(url)
 
 db = client.pytech
 
+ 
+Freddie = {
+    "student_id": "1007",
+    "first_name": "Freddie",
+    "last_name": "Krueger"
+}
+ 
+Jason = {
+    "student_id": "1008",
+    "first_name": "Jason",
+    "last_name": "Vorhees"
+}
+
+Ghost = {
+    "student_id": "1009",
+    "first_name": "Ghost",
+    "last_name": "Face"
+}
+ 
 students = db.students
+ 
+Freddie_student_id = students.insert_one(Freddie).inserted_id
+Jason_student_id = students.insert_one(Jason).inserted_id
+Ghost_student_id = students.insert_one(Ghost).inserted_id
 
-profiles = [
-    {
-        "first_name": "Freddie",
-        "last_name": "Krueger",
-        "student_id": "1007"
-    },
-    {
-        "first_name": "Jason",
-        "last_name": "Vorhees",
-        "student_id": "1008"
-    },
-    {
-        "first_name": "Ghost",
-        "last_name": "Face",
-        "student_id": "1009"
-    }
-]
 
-for profile in profiles:
-    new_student_Id = students.insert_one(profile).inserted_id
-    print(new_student_Id)
-    
+print("\n -- INSERT STATEMENTS --")
+print("  Inserted student record Freddie Krueger into the students collection with document_id " + str(Freddie_student_id))
+print("  Inserted student record Jason Vorhees into the students collection with document_id " + str(Jason_student_id))
+print("  Inserted student record Ghost Face into the students collection with document_id " + str(Ghost_student_id))
+
+input("\n  End of program, press the Enter key to exit... ")
