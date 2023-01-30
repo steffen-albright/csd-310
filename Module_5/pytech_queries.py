5b2fbd18dfb18ee40fc58b3b84f2cbeaea9b90e9
steffen-albright <104539115+steffen-albright@users.noreply.github.com>
2023-01-30 09:58:39 -0600

    Update pytech_queries.py

--- Module_5/pytech_queries.py
+++ Module_5/pytech_queries.py
@@ -1,22 +1,4 @@
-from pymongo import MongoClient
 
-url = "mongodb+srv://admin:admin@cluster0.7vyzi7i.mongodb.net/?retryWrites=true&w=majority"
-
-client = MongoClient(url)
-
-db = client.pytech
-
-students = db.students
-
-docs = students.find({})
-
-for doc in docs:
-	print(doc)
-
-
-doc = students.find_one({"student_id": "1007"})
-
-print(doc["student_id"])
 
 from pymongo import MongoClient
  
@@ -40,3 +22,5 @@ Freddie = students.find_one({"student_id": "1007"})
 print("\n  student document from the find_one() query  ")
 
 print("  Student ID: " + Freddie["student_id"] + "\n  First Name: " + Freddie["first_name"] + "\n  Last Name: " + Freddie["last_name"] + "\n")
+
+input("\n  End of program, press the Enter key to continue ... ")
