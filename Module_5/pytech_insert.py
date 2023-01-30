754450aeeb28f2c14b1b46fa34aff9ef3e32faed
steffen-albright <104539115+steffen-albright@users.noreply.github.com>
2023-01-30 09:03:00 -0600

    Create pytech_insert.py

---
+++ Module_5/pytech_insert.py
@@ -0,0 +1,32 @@
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.7vyzi7i.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(url)
+
+db = client.pytech
+
+students = db.students
+
+profiles = [
+    {
+        "first_name": "Fredie",
+        "last_name": "Krueger",
+        "student_id": "1009"
+    },
+    {
+        "first_name": "Jason",
+        "last_name": "Vorhees",
+        "student_id": "1008"
+    },
+    {
+        "first_name": "Ghost",
+        "last_name": "Face",
+        "student_id": "1009"
+    }
+]
+
+for profile in profiles:
+    new_student_Id = students.insert_one(profile).inserted_id
+    print(new_student_Id)
+    
