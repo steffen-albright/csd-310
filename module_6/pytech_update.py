5f619b3b96e18c2d0dc3c62881197c36ab778e87
steffen-albright <104539115+steffen-albright@users.noreply.github.com>
2023-02-04 19:14:07 -0600

    Update pytech_update.py

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -1,1 +1,26 @@
+from pymongo import MongoClient
+ 
+url = "mongodb+srv://admin:admin@cluster0.7vyzi7i.mongodb.net/?retryWrites=true&w=majority"
 
+client = MongoClient(url)
+
+db = client.pytech
+ 
+students = db.students
+ 
+student_list = students.find({})
+
+print("\n  -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+
+for doc in student_list:
+    print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
+
+result = students.update_one({"student_id": "1007"}, {"$set": {"last_name": "Krueger II"}})
+ 
+freddie = students.find_one({"student_id": "1007"})
+
+print("\n  -- DISPLAYING STUDENT DOCUMENT 1007 --")
+
+print("  Student ID: " + freddie["student_id"] + "\n  First Name: " + freddie["first_name"] + "\n  Last Name: " + freddie["last_name"] + "\n")
+ 
+input("\n  End of program, press the enter key to continue...")
